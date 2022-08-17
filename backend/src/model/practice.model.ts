9a75a9beaa5217d7edd0410c324aad2f62cc4212
mohammad fazeli <moohammadfazeli@gmail.com>
2022-08-17 23:18:03 +0430

    fix bug: remove practice in saved, after remove practice

--- backend/src/model/practice.model.ts
+++ backend/src/model/practice.model.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import AnswerModel from "./answer.model";
+import UserModel from "./user.model";
 import { removeFile } from "../utils/removeFile";
 
 export interface PracticeDocument extends mongoose.Document {
@@ -40,11 +41,20 @@ PracticeSchema.pre<PracticeDocument>("remove", async function (next) {
   if (this.file) {
     removeFile(`/public/files/practice/${this.file}`);
   }
-  // AnswerModel.deleteMany({ practice_id: this._id }).then(() => next());
   const answers = await AnswerModel.find({ practice_id: this._id });
   for (const answer of answers) {
     await answer.remove();
   }
+  const users = await UserModel.find({
+    saved: { $in: [{ _id: this._id, title: this.title }] },
+  });
+  for (const user of users) {
+    user.saved = user.saved.filter(
+      (saved) => saved._id.toString() !== this._id.toString()
+    );
+    await user.save();
+  }
+  next();
 });
 
 const Practice = mongoose.model<PracticeDocument>("Practice", PracticeSchema);
