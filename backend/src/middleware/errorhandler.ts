d2d063ac76c40d2fcc28549c8faed57f239ca671
mohammad fazeli <moohammadfazeli@gmail.com>
2022-09-09 16:24:22 +0430

    update errorhandler function

--- backend/src/middleware/errorhandler.ts
+++ backend/src/middleware/errorhandler.ts
@@ -2,13 +2,13 @@ import { Response } from "express";
 import { Request } from "../types/request";
 
 export default function errorhandler(req: Request, res: Response) {
-  console.log(req.error);
   if (req.error?.code) {
     return res.status(req.error.code).json({
       status: req.error.code,
       message: req.error.message,
     });
   }
+  console.log(req.error);
   return res.status(500).json({
     status: 500,
     message: "خطایی رخ داده است.",
